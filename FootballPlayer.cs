3571acd8d75e054c4e2832ca2d6e6c40c672f770
foxy29 <134440639+foxy29@users.noreply.github.com>
2023-05-24 02:22:30 +0300

    Update FootballPlayer.cs

--- FootballPlayer.cs
+++ FootballPlayer.cs
@@ -1,134 +1,56 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mail;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace homework
 {
-    public class Game
+    public abstract class FootballPlayer : Person
     {
-        private Team team1;
-        private Team team2;
-        private Referee referee;
-        private Referee assistent1;
-        private Referee assistent2;
-        private List<Goal> goals;
-        private string result;
-        private Team winner;
-
-        public Team Team1
+        private int number;
+        private double height;
+        public int Number
         {
-            get { return team1; }
+            get { return number; }
+            private set 
+            {
+                if (IsValidNumber(value) == true) { number = value; }
+                else { throw new ArgumentException("Number must be positive and between 1 and 100.");}
+            }
+        }
+        public double Height
+        {
+            get { return height; }
             private set
             {
-                if (IsValidTeam(value) == true) { team1 = value; }
-                else { throw new ArgumentException("The first team must consist of 11 players."); }
+                if(IsValidDouble(value) == true) { height = value; }
+                else { throw new ArgumentException("Height must be positive number and contain oly digits."); }
             }
         }
-        public Team Team2
+        public FootballPlayer(string name, int number, int age, double height ) 
+            :base(name,age)
         {
-            get { return team2; }
-            private set
-            {
-                if (IsValidTeam(value) == true) { team2 = value; }
-                else { throw new ArgumentException("The second team must consist of 11 players."); }
-            }
+            Number = number;
+            Height = height;
         }
-        public Referee Referee { get; private set; }
-        public Referee Assistent1 { get; private set; }
-        public Referee Assistent2 { get; private set; }
-        public List<Goal> Goals { get; private set; }
-        public string Result { get; private set; }
-        public Team Winner
+        public virtual int Attack()
         {
-            get
-            {
-                return PlayFootball();
-            }
+            int attackScore = 10;
+            return attackScore;
         }
 
-        public Game(Team teamA, Team teamB, Referee reff, Referee assistRef1,Referee assistRef2)
+        public virtual int Defend()
         {
-            Team1 = teamA;
-            Team2 = teamB;
-            Referee = reff;
-            Assistent1 = assistRef1;
-            Assistent2 = assistRef2;
+            int defendScore = 10;
+            return defendScore;
         }
-        private bool IsValidTeam(Team team)
+
+        protected bool IsValidDouble(double num)
         {
-            if (team.FootballPlayers.Count == 11) { return true; }
+            if ( num > 0) { return true; }
             else { return false; }
-        }
-        public Team PlayFootball()
-        {
-            
-            FootballPlayer attacker = GetRandomPlayer(Team1.FootballPlayers);
-            FootballPlayer defender = GetRandomPlayer(Team2.FootballPlayers);
-            bool team1HasBall = true;
-            List<Goal> goalsTeam1 = new List<Goal>();
-            List<Goal> goalsTeam2 = new List<Goal>();
-            int timer = 0;
-            do
-            {
-                int scoreA = attacker.Attack();
-                int scoreD = defender.Defend();
-
-                if (scoreA > scoreD) 
-                { 
-                    team1HasBall = true; 
-                }
-                else if(scoreA < scoreD) 
-                { 
-                    team1HasBall = false;
-                    FootballPlayer temp = attacker;
-                    attacker = defender;
-                    defender = temp;
-                }
-
-                if (attacker is Striker && defender is Goalkeeper)
-                {
-                    if(scoreA > scoreD)
-                    {
-                        Goal goal = new Goal(timer, attacker);
-                        Goals.Add(goal);
-                        if(team1HasBall)
-                        {
-                            goalsTeam1.Add(goal);
-                        }
-                        else
-                        {
-                            goalsTeam2.Add(goal);
-                        }
-                    }
-                    
-                }
-                timer++;
-
-            }
-            while (timer == 90);
-
-            this.Result = $"Team 1: {goalsTeam1.Count} - {goalsTeam2.Count} : Team 2";
-            Team winner;
-            if(goalsTeam1.Count > goalsTeam2.Count)
-            {
-                winner = Team1;
-            }
-            else
-            {
-                winner = Team2;
-            }
-
-            return winner;
-        }
-
-        private FootballPlayer GetRandomPlayer(List<FootballPlayer> players)
-        {
-            Random random = new Random();
-            int randomPlayer = random.Next(players.Count);
-            return players[randomPlayer];
         }
 
     }
